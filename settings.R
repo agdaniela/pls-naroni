62f8e31273163aa66c7d419ef93efb4769696563
girelaignacio <girelaignacio@gmail.com>
2023-05-21 00:29:13 -0300

    fit PLS

--- settings.R
+++ settings.R
@@ -5,19 +5,20 @@ source("kfold_cv_selection.R")
 
 target <- "MPI"
 pls.directions <- 5
+nfolds <- 5
 
 # Select dataframe ####
 df <- selectdfs(plsdata,3)
 
 # Train and Test split ####
-data <- random.split(df, 0.7)
+data <- random.split(df, 0.5)
 
 ytrain <- data$data_train[, target]; Xtrain <- data$data_train[,-c(1:6)]
 
 ytest <- data$data_test[, target]; Xtest <- data$data_test[,-c(1:6)]
 
 # Hyperparameter selection ####
-hyperparam <- kfoldCV.selection(Xtrain, ytrain, 5, pls.directions)
+hyperparam <- kfoldCV.selection(Xtrain, ytrain, nfolds, pls.directions)
 
 # Estimate model ####
   # Linear model
@@ -38,9 +39,14 @@ ytest_pred.lasso <- glmnet::predict.glmnet(lasso.fit, as.matrix(newdata))
 
   # PLS model
     # Fit model
-pls.fit <- chemometrics::pls1_nipals(Xtrain, ytrain, a = hyperparam$d.min)
+pls.projections <- chemometrics::pls1_nipals(Xtrain, ytrain, a = hyperparam$d.min)
+data <- as.data.frame(cbind(ytrain, as.matrix(Xtrain) %*% pls.projections$W))
+pls.fit <- lm(ytrain ~ . , data)
+
+newdata <- data.frame(as.matrix(Xtest) %*% pls.projections$W)
+colnames(newdata) <- colnames(data)[-1]
     # Predict over Xtest
-ytest_pred.pls <- as.matrix(Xtest) %*% pls.fit$W
+ytest_pred.pls <- predict(pls.fit, newdata)
 
 # Test results ####
 mean((ytest-ytest_pred.lm)^2)
